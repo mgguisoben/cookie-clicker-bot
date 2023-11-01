8bb20edeee8f88975a07e053871d21753bde7dc5
mgguisoben <mgguisoben@gmail.com>
2023-11-01 12:13:01 +0800

    Basic bot done

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def upgrade():
-    pass
+def get_upgrade():
+    upgrade_types = ["Cursor", "Grandma", "Factory", "Mine", "Shipment", "Alchemy\ lab", "Portal", "Time\ machine"]
+    upgrades = []
+    prices = []
+
+    for upgrade in upgrade_types:
+        upgrade_type = driver.find_element(By.CSS_SELECTOR, value=f"#buy{upgrade} > b:nth-child(1)")
+        upgrades.append(upgrade_type)
+        prices.append(upgrade_type.text.split(" - ")[1].replace(",", ""))
+
+    return {upgrades[i]: prices[i] for i in range(len(upgrades))}
 
 
 driver = webdriver.Firefox()
@@ -11,9 +22,20 @@ driver.get("http://orteil.dashnet.org/experiments/cookie/ ")
 
 cookie = driver.find_element(By.ID, value="cookie")
 
-# while True:
-#     cookie.click()
+timeout = time.time() + 5
 
+while True:
+    cookie.click()
+
+    if time.time() > timeout:
+        ups = get_upgrade()
+        cookie_count = driver.find_element(By.ID, value="money").text
+        for up, price in reversed(ups.items()):
+            if int(cookie_count) > int(price):
+                up.click()
+                continue
+
+        timeout = time.time() + 5
 
 # driver.close() # Closes single tab
 # driver.quit() # Shutdown entire browser
