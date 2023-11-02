1b5042704a1562180d83425940f605812867204f
mgguisoben <mgguisoben@gmail.com>
2023-11-02 08:28:26 +0800

    Cookie Clicker Bot done

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ def get_upgrade():
     prices = []
 
     for upgrade in upgrade_types:
-        upgrade_type = driver.find_element(By.CSS_SELECTOR, value=f"#buy{upgrade} > b:nth-child(1)")
+        upgrade_type = driver.find_element(By.CSS_SELECTOR, value=f"#buy{upgrade}")
+        price = driver.find_element(By.CSS_SELECTOR, value=f"#buy{upgrade} > b:nth-child(1)")
         upgrades.append(upgrade_type)
-        prices.append(upgrade_type.text.split(" - ")[1].replace(",", ""))
+        prices.append(price.text.split(" - ")[1].replace(",", ""))
 
     return {upgrades[i]: prices[i] for i in range(len(upgrades))}
 
@@ -21,21 +22,20 @@ driver = webdriver.Firefox()
 driver.get("http://orteil.dashnet.org/experiments/cookie/ ")
 
 cookie = driver.find_element(By.ID, value="cookie")
-
-timeout = time.time() + 5
+timeout = time.time() + 1
 
 while True:
     cookie.click()
 
     if time.time() > timeout:
         ups = get_upgrade()
-        cookie_count = driver.find_element(By.ID, value="money").text
+        cookie_count = driver.find_element(By.ID, value="money").text.replace(",", "")
         for up, price in reversed(ups.items()):
             if int(cookie_count) > int(price):
                 up.click()
-                continue
+                break
 
-        timeout = time.time() + 5
+        timeout = time.time() + 1
 
 # driver.close() # Closes single tab
 # driver.quit() # Shutdown entire browser
